cb5cab6dfa9e7b1b0206a96fb92c08f85db49951
Knight <790184126@qq.com>
2018-10-11 21:03:17 +0800

    basic work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![feature(const_fn)]
+
+use std::f32;
 use std::ops;
 #[derive(Copy, Clone)]
 struct Vector3 {
@@ -5,87 +8,254 @@ struct Vector3 {
     y: f32,
     z: f32,
 }
-impl Vector3{
-    fn new(x:f32, y:f32, z:f32)->Vector3{
-        Vector3{x:x,y:y,z:z}
+impl Vector3 {
+    const fn new(x: f32, y: f32, z: f32) -> Vector3 {
+        Vector3 { x: x, y: y, z: z }
     }
-    fn normalize(self)->Self{
-        self*(1.0/(self.x*self.x+self.y*self.y+self.z*self.z).sqrt())
+    fn normalize(self) -> Self {
+        self * (1.0 / (self.x * self.x + self.y * self.y + self.z * self.z).sqrt())
     }
-    fn dot(self, other:Vector3)->f32{
-        self.x * other.x + self.y * other.y + self.z *other.z
+    fn dot(self, other: Vector3) -> f32 {
+        self.x * other.x + self.y * other.y + self.z * other.z
     }
-    fn cross(self, other:Vector3)->Vector3{
-        Vector3::new(self.y * other.z - self.z * other.y, self.z*other.x - self.x * other.z, self.x * other.y - self.y * other.x)
+    fn cross(self, other: Vector3) -> Vector3 {
+        Vector3::new(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x,
+        )
+    }
+    fn length(self) -> f32 {
+        (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
     }
 }
-impl ops::Add<Vector3> for Vector3{
+impl ops::Add<Vector3> for Vector3 {
     type Output = Vector3;
-    fn add(self, rhs:Vector3)->Vector3{
+    fn add(self, rhs: Vector3) -> Vector3 {
         Vector3::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
     }
 }
-impl ops::Mul<Vector3> for Vector3{
+impl ops::Mul<Vector3> for Vector3 {
     type Output = Vector3;
-    fn mul(self, rhs:Vector3)->Vector3{
+    fn mul(self, rhs: Vector3) -> Vector3 {
         Vector3::new(self.x * rhs.x, self.y * rhs.y, self.z * rhs.z)
     }
 }
-impl ops::Mul<f32> for Vector3{
+impl ops::Mul<f32> for Vector3 {
     type Output = Vector3;
-    fn mul(self, rhs:f32)->Vector3{
+    fn mul(self, rhs: f32) -> Vector3 {
         Vector3::new(self.x * rhs, self.y * rhs, self.z * rhs)
     }
 }
-impl ops::Mul<Vector3> for f32{
+impl ops::Mul<Vector3> for f32 {
     type Output = Vector3;
-    fn mul(self, rhs:Vector3)->Vector3{
+    fn mul(self, rhs: Vector3) -> Vector3 {
         Vector3::new(rhs.x * self, rhs.y * self, rhs.z * self)
     }
 }
-impl ops::Sub<Vector3> for Vector3{
+impl ops::Sub<Vector3> for Vector3 {
     type Output = Vector3;
-    fn sub(self, rhs: Vector3) -> Vector3{
+    fn sub(self, rhs: Vector3) -> Vector3 {
         Vector3::new(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z)
     }
 }
-struct Ray{
-    origin:Vector3,
-    direction:Vector3
+
+#[derive(Copy, Clone)]
+struct Ray {
+    origin: Vector3,
+    direction: Vector3,
 }
-impl Ray{
-    fn new(origin:Vector3, direction:Vector3)->Ray{
-        Ray{origin:origin, direction:direction}
+
+impl Ray {
+    fn new(origin: Vector3, direction: Vector3) -> Ray {
+        Ray {
+            origin: origin,
+            direction: direction.normalize(),
+        }
     }
 }
-enum ReflectionType{
-    diffuse,
-    specular,
-    refractable
+
+enum ReflectionType {
+    Diffuse,
+    Specular,
+    Refractable,
 }
-struct Sphere{
-    radius:f32,
-    position:Vector3,
-    emission:Vector3,
-    color:Vector3,
-    reflectionType:ReflectionType
+trait Intersectable {
+    fn intersect(&self, ray: &Ray) -> f32;
 }
-//  struct Ray { Vec o, d; Ray(Vec o_, Vec d_) : o(o_), d(d_) {} };
-//  enum Refl_t { DIFF, SPEC, REFR };  // material types, used in radiance()
-//  struct Sphere {
-//    double rad;       // radius
-//    Vec p, e, c;      // position, emission, color
-//    Refl_t refl;      // reflection type (DIFFuse, SPECular, REFRactive)
-//    Sphere(double rad_, Vec p_, Vec e_, Vec c_, Refl_t refl_):
-//      rad(rad_), p(p_), e(e_), c(c_), refl(refl_) {}
-//    double intersect(const Ray &r) const { // returns distance, 0 if nohit
-//      Vec op = p-r.o; // Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
-//      double t, eps=1e-4, b=op.dot(r.d), det=b*b-op.dot(op)+rad*rad;
-//      if (det<0) return 0; else det=sqrt(det);
-//      return (t=b-det)>eps ? t : ((t=b+det)>eps ? t : 0);
-//    }
-//  };
+struct Sphere {
+    radius: f32,
+    position: Vector3,
+    emission: Vector3,
+    color: Vector3,
+    reflection_type: ReflectionType,
+}
+impl Sphere {
+    const fn new(
+        radius: f32,
+        position: Vector3,
+        emission: Vector3,
+        color: Vector3,
+        reflection_type: ReflectionType,
+    ) -> Sphere {
+        Sphere {
+            radius: radius,
+            position: position,
+            emission: emission,
+            color: color,
+            reflection_type: reflection_type,
+        }
+    }
+}
+impl Intersectable for Sphere {
+    fn intersect(&self, ray: &Ray) -> f32 {
+        let origin_to_position: Vector3 = self.position - ray.origin;
+        let project_to_direction: f32 = origin_to_position.dot(ray.direction);
+        let project_point: Vector3 = ray.origin + ray.direction * project_to_direction;
+        let length_to_project_point: f32 = (project_point - self.position).length();
+        if (length_to_project_point + f32::EPSILON) > self.radius {
+            return 0.0;
+        } else {
+            return (self.radius * self.radius - length_to_project_point * length_to_project_point)
+                .sqrt();
+        }
+    }
+}
+
+struct Scene {
+    objects: Vec<Box<Intersectable>>,
+}
+
+impl Scene {
+    fn new() -> Scene {
+        Scene {
+            objects: Vec::<Box<Intersectable>>::new(),
+        }
+    }
+    fn push(&mut self, object: Box<Intersectable>) {
+        self.objects.push(object);
+    }
+
+    fn intersect(&self, _ray: &Ray, _t: &mut f32, _id: &mut usize) -> bool {
+        let count = self.objects.len();
+        let mut distance: f32 = 0.0;
+        *_t = f32::INFINITY;
+        for i in count - 1..0 {
+            let object = &self.objects[i];
+            distance = object.intersect(_ray);
+            if (distance - 0.0).abs() > f32::EPSILON && distance < (*_t) {
+                *_id = i;
+                *_t = distance;
+            }
+        }
+        *_t < f32::INFINITY
+    }
+
+    fn shade(&self, ray: &Ray, depth: i32) -> Vector3 {
+        let mut t: f32 = 0.0;
+        let mut id: usize = 0;
+        if !self.intersect(ray, &mut t, &mut id) {
+            return Vector3::new(1.0, 1.0, 1.0);
+        }
+        Vector3::new(0.0, 0.0, 0.0)
+    }
+}
+
+fn clamp(x: f32) -> f32 {
+    if x < 0.0 {
+        return 0.0;
+    } else if x > 1.0 {
+        return 1.0;
+    }
+    x
+}
+fn to_int(x: f32) -> i32 {
+    (clamp(x).powf(1.0 / 2.2) * 255.0 + 0.5) as i32
+}
 
 fn main() {
-    println!("Hello, world!");
+    let width = 1024;
+    let height = 768;
+    let samps = 1;
+
+    let mut scene = Scene::new();
+    scene.push(Box::new(Sphere::new(
+        1e5,
+        Vector3::new(1e5 + 1.0, 40.8, 81.6),
+        Vector3::new(0.0, 0.0, 0.0),
+        Vector3::new(0.75, 0.25, 0.25),
+        ReflectionType::Diffuse,
+    )));
+    scene.push(Box::new(Sphere::new(
+        1e5,
+        Vector3::new(-1e5 + 99.0, 40.8, 81.6),
+        Vector3::new(0.0, 0.0, 0.0),
+        Vector3::new(0.25, 0.25, 0.75),
+        ReflectionType::Diffuse,
+    )));
+    scene.push(Box::new(Sphere::new(
+        1e5,
+        Vector3::new(50.0, 40.8, 1e5),
+        Vector3::new(0.0, 0.0, 0.0),
+        Vector3::new(0.75, 0.75, 0.75),
+        ReflectionType::Diffuse,
+    )));
+    scene.push(Box::new(Sphere::new(
+        1e5,
+        Vector3::new(50.0, 40.8, -1e5 + 170.0),
+        Vector3::new(0.0, 0.0, 0.0),
+        Vector3::new(0.0, 0.0, 0.0),
+        ReflectionType::Diffuse,
+    )));
+    scene.push(Box::new(Sphere::new(
+        1e5,
+        Vector3::new(50.0, 1e5, 81.6),
+        Vector3::new(0.0, 0.0, 0.0),
+        Vector3::new(0.75, 0.75, 0.75),
+        ReflectionType::Diffuse,
+    )));
+    scene.push(Box::new(Sphere::new(
+        1e5,
+        Vector3::new(50.0, -1e5 + 81.6, 81.6),
+        Vector3::new(0.0, 0.0, 0.0),
+        Vector3::new(0.75, 0.75, 0.75),
+        ReflectionType::Diffuse,
+    )));
+    scene.push(Box::new(Sphere::new(
+        16.5,
+        Vector3::new(27.0, 16.5, 47.0),
+        Vector3::new(0.0, 0.0, 0.0),
+        Vector3::new(1.0, 1.0, 1.0),
+        ReflectionType::Specular,
+    )));
+    scene.push(Box::new(Sphere::new(
+        16.5,
+        Vector3::new(73.0, 16.5, 78.0),
+        Vector3::new(0.0, 0.0, 0.0),
+        Vector3::new(1.0, 1.0, 1.0),
+        ReflectionType::Refractable,
+    )));
+    scene.push(Box::new(Sphere::new(
+        600.0,
+        Vector3::new(50.0, 681.6 - 0.27, 81.6),
+        Vector3::new(12.0, 12.0, 12.0),
+        Vector3::new(0.0, 0.0, 0.0),
+        ReflectionType::Diffuse,
+    )));
+
+    let camera_ray = Ray::new(
+        Vector3::new(50.0, 52.0, 295.6),
+        Vector3::new(0.0, -0.042612, -1.0).normalize(),
+    );
+    let cx = Vector3::new(
+        width as f32 * 0.5135 / height as f32,
+        width as f32 * 0.5135 / height as f32,
+        width as f32 * 0.5135 / height as f32,
+    );
+    let cy = cx.cross(camera_ray.direction).normalize() * 0.5135;
+    let result = Vector3::new(0.0, 0.0, 0.0);
+    let context = vec![Vector3::new(0.0, 0.0, 0.0); width * height];
+    for y in 0..height - 1 {
+        for x in 0..width - 1 {}
+    }
 }
